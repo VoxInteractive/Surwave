c8f19f9a6961ea4e595fb960ee13d105122aae47
Alp <gokalp84@gmail.com>
2025-11-05 20:43:40 +1100

    borders

--- scenes/Stage 01/terrain.gd
+++
@@ -1,7 +0,0 @@
-extends MeshInstance2D
-
-@export_range(6, 12, 1) var size_exponent :int = 8
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
