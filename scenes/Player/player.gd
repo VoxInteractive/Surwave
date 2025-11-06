b9532f5a79179ba098c54a73801970988df48c7f
Alp <gokalp84@gmail.com>
2025-11-06 13:07:01 +1100

    Foundations (#1)
    * a few initial sprites a nodes for the player & tilemaps
    
    * noise texture terrain
    
    * borders
    
    * add some collision shapes
    
    * portal & altar scenes
    
    * camera
    
    * a bit of node & node group reorg. add stage script

---
+++ scenes/Player/player.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+
+@export_group("Movement")
+@export_range(1.0, 100.0, 1.0)
+var movement_speed :float = 20.0
+
+var input_movement_vector :Vector2
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	input_movement_vector = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	velocity = input_movement_vector * movement_speed
+	move_and_slide()
