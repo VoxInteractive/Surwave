0ae0aadf912fa8d1d983be03361f7242eb3a80ef
Alp Ozcan <gokalp84@gmail.com>
2025-11-08 13:26:29 +1100

    implement AnimatedObject to enable code reuse

--- Game/scenes/Altar/altar.gd
+++ Game/scenes/Altar/altar.gd
@@ -1,10 +1,9 @@
 class_name Altar extends AnimatedObject
 
 enum AltarState {
-	AVAILABLE,
-	SPENT
+	AVAILABLE, # 0
+	SPENT # 1
 }
-var state: AltarState = AltarState.AVAILABLE
 
 const AltarAnimationFrames: Dictionary[AltarState, Array] = {
 	AltarState.AVAILABLE: [0, 1, 2, 3, 4, 5],
@@ -14,45 +13,22 @@ const AltarAnimationModes: Dictionary[AltarState, Game.AnimationMode] = {
 	AltarState.AVAILABLE: Game.AnimationMode.LOOP,
 	AltarState.SPENT: Game.AnimationMode.ONCE
 }
-var animation_interval: float = 0.25
-var _animation_frames: Array = AltarAnimationFrames[AltarState.AVAILABLE]
-var _animation_timer: float = 0.0
-var _animation_current_frame: int = 0 # index of the current frame within the particular ...AnimationFrames array
-
-@onready var sprite: Sprite2D = $StaticBody2D/Sprite2D
 
 
-func set_state(new_state: AltarState) -> void:
-	if state == new_state: return
-	state = new_state
-	_animation_frames = AltarAnimationFrames[state]
-	_animation_current_frame = 0
-	_animation_timer = 0.0
+func _get_animation_frames(state: AltarState) -> Array:
+	return AltarAnimationFrames[state]
 
 
-func _animate(delta: float) -> void:
-	_animation_timer += delta
-	if ((AltarAnimationModes[state] == Game.AnimationMode.ONCE and _animation_current_frame >= _animation_frames.size() - 1)
-		or _animation_timer < animation_interval):
-			return
-	
-	var frame_count: int = _animation_frames.size()
-	if _animation_timer >= animation_interval:
-		_animation_timer = 0.0
-		_animation_current_frame = (_animation_current_frame + 1) % frame_count
-		sprite.frame = _animation_frames[_animation_current_frame]
+func _get_animation_mode(state: AltarState):
+	return AltarAnimationModes[state]
 
 
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-func _process(delta: float) -> void:
-	_animate(delta)
+	set_state(AltarState.AVAILABLE)
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body.is_in_group("Players") and state == AltarState.AVAILABLE:
+	if body.is_in_group("Players") and _current_state == AltarState.AVAILABLE:
 		# TODO: Implement upgrade logic
 		print("Player upgraded!")
 		set_state(AltarState.SPENT)
