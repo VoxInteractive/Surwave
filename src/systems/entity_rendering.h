8c3dd2319e757f736a0b6fc2656177fe26d456ac
Alp <gokalp84@gmail.com>
2025-11-13 00:09:57 +1100

    14ms after refactoring

--- src/systems/entity_rendering.h
+++ src/systems/entity_rendering.h
@@ -69,73 +69,6 @@ namespace {
         buffer.set(cursor++, color.a);
     }
 
-    template <typename TransformType>
-    void update_multimesh_buffer(
-        RenderingServer* rendering_server,
-        const MultiMeshRenderer& renderer,
-        const std::vector<TransformType>& transforms,
-        const std::vector<Color>& instance_colors,
-        const std::vector<Color>& instance_custom_data)
-    {
-        size_t instance_count = std::min(transforms.size(), renderer.instance_count);
-
-        size_t floats_per_instance = 0;
-        if (renderer.transform_format == godot::MultiMesh::TRANSFORM_2D)
-        {
-            floats_per_instance = 8;
-        }
-        else // TRANSFORM_3D
-        {
-            floats_per_instance = 12;
-        }
-
-        floats_per_instance += (renderer.use_colors ? 4 : 0) + (renderer.use_custom_data ? 4 : 0);
-
-        PackedFloat32Array& buffer = g_multimesh_buffer_cache[renderer.rid];
-        size_t required_size = renderer.instance_count * floats_per_instance;
-        if (buffer.size() != required_size) {
-            UtilityFunctions::push_warning(
-                godot::String("Entity Rendering (MultiMesh): Resizing MultiMesh buffer from ") +
-                godot::String::num_int64(buffer.size()) + godot::String(" to ") +
-                godot::String::num_int64(required_size));
-            buffer.resize(required_size);
-        }
-
-        float* buffer_ptr = buffer.ptrw();
-        size_t buffer_cursor = 0;
-        for (size_t instance_idx = 0; instance_idx < instance_count; ++instance_idx) {
-            const TransformType& t = transforms[instance_idx];
-
-            if constexpr (std::is_same_v<TransformType, Transform2D>) {
-                buffer_ptr[buffer_cursor++] = t.columns[0].x; buffer_ptr[buffer_cursor++] = t.columns[1].x; buffer_ptr[buffer_cursor++] = 0.0f; buffer_ptr[buffer_cursor++] = t.columns[2].x; // x row
-                buffer_ptr[buffer_cursor++] = t.columns[0].y; buffer_ptr[buffer_cursor++] = t.columns[1].y; buffer_ptr[buffer_cursor++] = 0.0f; buffer_ptr[buffer_cursor++] = t.columns[2].y; // y row
-            }
-            else if constexpr (std::is_same_v<TransformType, Transform3D>) {
-                buffer_ptr[buffer_cursor++] = t.basis.rows[0][0]; buffer_ptr[buffer_cursor++] = t.basis.rows[0][1]; buffer_ptr[buffer_cursor++] = t.basis.rows[0][2]; buffer_ptr[buffer_cursor++] = t.origin.x;
-                buffer_ptr[buffer_cursor++] = t.basis.rows[1][0]; buffer_ptr[buffer_cursor++] = t.basis.rows[1][1]; buffer_ptr[buffer_cursor++] = t.basis.rows[1][2]; buffer_ptr[buffer_cursor++] = t.origin.y;
-                buffer_ptr[buffer_cursor++] = t.basis.rows[2][0]; buffer_ptr[buffer_cursor++] = t.basis.rows[2][1]; buffer_ptr[buffer_cursor++] = t.basis.rows[2][2]; buffer_ptr[buffer_cursor++] = t.origin.z;
-            }
-
-            if (renderer.use_colors && !instance_colors.empty()) {
-                const Color& color = instance_colors[instance_idx % instance_colors.size()];
-                buffer_ptr[buffer_cursor++] = color.r;
-                buffer_ptr[buffer_cursor++] = color.g;
-                buffer_ptr[buffer_cursor++] = color.b;
-                buffer_ptr[buffer_cursor++] = color.a;
-            }
-
-            if (renderer.use_custom_data && !instance_custom_data.empty()) {
-                const Color& color = instance_custom_data[instance_idx % instance_custom_data.size()];
-                buffer_ptr[buffer_cursor++] = color.r;
-                buffer_ptr[buffer_cursor++] = color.g;
-                buffer_ptr[buffer_cursor++] = color.b;
-                buffer_ptr[buffer_cursor++] = color.a;
-            }
-        }
-
-        rendering_server->multimesh_set_buffer(renderer.rid, buffer);
-        rendering_server->multimesh_set_visible_instances(renderer.rid, instance_count);
-    }
 }
 
 // Collect instances for a single prefab and update the corresponding multimesh buffer.
@@ -146,49 +79,67 @@ void update_renderer_for_prefab(
     RenderingServer* rendering_server,
     const MultiMeshRenderer& renderer)
 {
-    // Get the total number of entities that will be rendered by this query.
-    // This allows us to pre-allocate memory for the vectors, avoiding reallocations.
-    size_t total_instances = renderer.query.count();
-
-    std::vector<TransformType> transforms;
-    transforms.reserve(total_instances);
-
-    std::vector<Color> instance_colors;
-    if (renderer.use_colors) {
-        instance_colors.reserve(total_instances);
+    size_t floats_per_instance = 0;
+    if (renderer.transform_format == godot::MultiMesh::TRANSFORM_2D) {
+        floats_per_instance = 8;
+    }
+    else { // TRANSFORM_3D
+        floats_per_instance = 12;
     }
 
-    std::vector<Color> instance_custom_data;
-    if (renderer.use_custom_data) {
-        instance_custom_data.reserve(total_instances);
+    floats_per_instance += (renderer.use_colors ? 4 : 0) + (renderer.use_custom_data ? 4 : 0);
+
+    PackedFloat32Array& buffer = g_multimesh_buffer_cache[renderer.rid];
+    size_t required_size = renderer.instance_count * floats_per_instance;
+    if (buffer.size() != required_size) {
+        UtilityFunctions::push_warning(
+            godot::String("Entity Rendering (MultiMesh): Resizing MultiMesh buffer from ") +
+            godot::String::num_int64(buffer.size()) + godot::String(" to ") +
+            godot::String::num_int64(required_size));
+        buffer.resize(required_size);
     }
+
+    float* buffer_ptr = buffer.ptrw();
+    size_t instance_count = 0;
 
     renderer.query.run([&](flecs::iter& it) {
         while (it.next()) {
             auto transform_field = it.field<const TransformType>(0);
-            for (auto i : it) {
-                transforms.push_back(transform_field[i]);
-            }
 
-            int field_index = 1;
-            if (renderer.use_colors) {
-                auto color_field = it.field<const RenderingColor>(field_index++);
-                for (auto i : it) {
-                    const RenderingColor& c = color_field[i];
-                    instance_colors.push_back(Color(c.r, c.g, c.b, c.a));
+            for (auto i : it) {
+                if (instance_count >= renderer.instance_count) {
+                    break;
                 }
-            }
-            if (renderer.use_custom_data) {
-                auto custom_data_field = it.field<const RenderingCustomData>(field_index);
-                for (auto i : it) {
-                    const RenderingCustomData& c = custom_data_field[i];
-                    instance_custom_data.push_back(Color(c.r, c.g, c.b, c.a));
+
+                size_t buffer_cursor = instance_count * floats_per_instance;
+                const TransformType& t = transform_field[i];
+
+                if constexpr (std::is_same_v<TransformType, Transform2D>) {
+                    buffer_ptr[buffer_cursor++] = t.columns[0].x; buffer_ptr[buffer_cursor++] = t.columns[1].x; buffer_ptr[buffer_cursor++] = 0.0f; buffer_ptr[buffer_cursor++] = t.columns[2].x;
+                    buffer_ptr[buffer_cursor++] = t.columns[0].y; buffer_ptr[buffer_cursor++] = t.columns[1].y; buffer_ptr[buffer_cursor++] = 0.0f; buffer_ptr[buffer_cursor++] = t.columns[2].y;
                 }
+                else if constexpr (std::is_same_v<TransformType, Transform3D>) {
+                    buffer_ptr[buffer_cursor++] = t.basis.rows[0][0]; buffer_ptr[buffer_cursor++] = t.basis.rows[0][1]; buffer_ptr[buffer_cursor++] = t.basis.rows[0][2]; buffer_ptr[buffer_cursor++] = t.origin.x;
+                    buffer_ptr[buffer_cursor++] = t.basis.rows[1][0]; buffer_ptr[buffer_cursor++] = t.basis.rows[1][1]; buffer_ptr[buffer_cursor++] = t.basis.rows[1][2]; buffer_ptr[buffer_cursor++] = t.origin.y;
+                    buffer_ptr[buffer_cursor++] = t.basis.rows[2][0]; buffer_ptr[buffer_cursor++] = t.basis.rows[2][1]; buffer_ptr[buffer_cursor++] = t.basis.rows[2][2]; buffer_ptr[buffer_cursor++] = t.origin.z;
+                }
+
+                if (renderer.use_colors) {
+                    const RenderingColor& c = it.field<const RenderingColor>(1)[i];
+                    buffer_ptr[buffer_cursor++] = c.r; buffer_ptr[buffer_cursor++] = c.g; buffer_ptr[buffer_cursor++] = c.b; buffer_ptr[buffer_cursor++] = c.a;
+                }
+                if (renderer.use_custom_data) {
+                    const int field_idx = renderer.use_colors ? 2 : 1;
+                    const RenderingCustomData& c = it.field<const RenderingCustomData>(field_idx)[i];
+                    buffer_ptr[buffer_cursor++] = c.r; buffer_ptr[buffer_cursor++] = c.g; buffer_ptr[buffer_cursor++] = c.b; buffer_ptr[buffer_cursor++] = c.a;
+                }
+                instance_count++;
             }
         }
     });
 
-    update_multimesh_buffer<TransformType>(rendering_server, renderer, transforms, instance_colors, instance_custom_data);
+    rendering_server->multimesh_set_buffer(renderer.rid, buffer);
+    rendering_server->multimesh_set_visible_instances(renderer.rid, instance_count);
 }
 
 
