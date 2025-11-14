8873e15ce1d1d4180496e4f52997aaca8a244c5f
Alp <gokalp84@gmail.com>
2025-11-14 13:47:20 +1100

    script improvements

--- scripts/build-release.sh
+++ scripts/build-release.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -ex
 
+COMMON_BUILD_ARGS=("target=template_release" "production=yes" "optimize=speed" "lto=full")
+
+
 EMSDK_DIR="$HOME/src/emsdk"
 source "${EMSDK_DIR}/emsdk_env.sh"
 export PATH="${EMSDK_DIR}:${PATH}"
@@ -8,6 +11,6 @@ export PATH="${EMSDK_DIR}/node/22.16.0_64bit/bin:${PATH}"
 export PATH="${EMSDK_DIR}/upstream/emscripten:${PATH}"
 
 for platform in web linux windows; do
-    scons --clean platform="${platform}" target=template_release
-    scons platform="${platform}" target=template_release
+    scons ${COMMON_BUILD_ARGS[@]} platform="${platform}" --clean
+    scons ${COMMON_BUILD_ARGS[@]} platform="${platform}"
 done
