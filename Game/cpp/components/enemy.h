ba10b4acc65f74ec210e321960cc8fcbcdd75927
Alp <gokalp84@gmail.com>
2025-11-15 15:02:14 +1100

    minor reorg of enemy data

--- Game/cpp/components/enemy.h
+++ Game/cpp/components/enemy.h
@@ -5,6 +5,7 @@
 enum class EnemyState {
     IDLE,
     RUNNING,
+    ATTACKING,
     DYING,
     DEAD
 };
@@ -13,6 +14,7 @@ inline FlecsRegistry register_enemy_components([](flecs::world& world) {
     world.component<EnemyState>()
         .constant("IDLE", EnemyState::IDLE)
         .constant("RUNNING", EnemyState::RUNNING)
+        .constant("ATTACKING", EnemyState::ATTACKING)
         .constant("DYING", EnemyState::DYING)
         .constant("DEAD", EnemyState::DEAD);
 });
