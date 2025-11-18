aa2b055f29d27b15dd03a11ec4a9a173dbc08ad8
Alp Ozcan <gokalp84@gmail.com>
2025-11-18 15:45:47 +1100

    use relative paths in scripts loader

--- scripts/build-release.sh
+++ scripts/build-release.sh
@@ -12,11 +12,9 @@ export PATH="${EMSDK_DIR}/upstream/emscripten:${PATH}"
 
 # Build for web with specific linkflags - https://www.flecs.dev/flecs/md_docs_2Quickstart.html#emscripten
 WEB_LINKFLAGS=("-s ALLOW_MEMORY_GROWTH=1" "-s STACK_SIZE=1mb" "-s EXPORTED_RUNTIME_METHODS=cwrap" "-s MODULARIZE=1" "-s EXPORT_NAME='Game'")
-scons "${COMMON_BUILD_ARGS[@]}" platform="web" linkflags="${WEB_LINKFLAGS[*]}" --clean
 scons "${COMMON_BUILD_ARGS[@]}" platform="web" linkflags="${WEB_LINKFLAGS[*]}"
 
 # Build for other platforms
 for platform in linux windows; do
-    scons ${COMMON_BUILD_ARGS[@]} platform="${platform}" --clean
     scons ${COMMON_BUILD_ARGS[@]} platform="${platform}"
 done
