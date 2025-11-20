77cc0451962df4d498f13cbf8c477d1a81d7224b
Alp <gokalp84@gmail.com>
2025-11-20 22:19:11 +1100

    Implement Y-sorting for the entity rendering system (#10)
    * center the player sprite & shape
    
    * Implement Y-sorting for the entity rendering system
    
    * tune enemy-player contact margins

--- Game/cpp/components/singletons.h
+++ Game/cpp/components/singletons.h
@@ -16,17 +16,17 @@ struct CharacterContactEndDistanceSquared {
 
 
 inline FlecsRegistry register_game_singleton_components([](flecs::world& world) {
-    const godot::real_t EnemyRadius = 8.0f;
-    const godot::real_t PlayerRadius = 7.0f;
-    const godot::real_t Hysteresis = 1.0f; // This creates a stable region where the enemy will remain in the Attacking state, preventing flapping.
+    const godot::real_t enemy_radius = 8.0f;
+    const godot::real_t contact_margin = 2.0f;
+    const godot::real_t hysteresis = 2.0f; // This creates a stable region where the enemy will remain in the Attacking state, preventing flapping.
 
     world.component<CharacterContactBeginDistanceSquared>("CharacterContactBeginDistanceSquared")
         .member<godot::real_t>("value")
         .add(flecs::Singleton)
-        .set<CharacterContactBeginDistanceSquared>({ godot::Math::pow(EnemyRadius + PlayerRadius, 2.0f) });
+        .set<CharacterContactBeginDistanceSquared>({ godot::Math::pow(enemy_radius + contact_margin, 2.0f) });
 
     world.component<CharacterContactEndDistanceSquared>("CharacterContactEndDistanceSquared")
         .member<godot::real_t>("value")
         .add(flecs::Singleton)
-        .set<CharacterContactEndDistanceSquared>({ godot::Math::pow(EnemyRadius + PlayerRadius + Hysteresis, 2.0f) });
+        .set<CharacterContactEndDistanceSquared>({ godot::Math::pow(enemy_radius + contact_margin + hysteresis, 2.0f) });
 });
