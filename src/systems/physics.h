50f8cfaa878e1e6100d1648025bbea480964a78a
Alp <gokalp84@gmail.com>
2025-11-20 13:34:01 +1100

    deduplicate 2D&3D physics systems

--- src/systems/physics.h
+++ src/systems/physics.h
@@ -6,6 +6,7 @@
 #include <godot_cpp/variant/transform3d.hpp>
 #include <godot_cpp/variant/variant.hpp>
 #include <godot_cpp/variant/vector2.hpp>
+#include <godot_cpp/variant/vector3.hpp>
 
 #include "src/components/physics.h"
 #include "src/components/transform.h"
@@ -92,127 +93,255 @@ namespace
             physics_server->free_rid(instance.body_rid);
         });
     }
+
+    template<
+        typename ShapesT,
+        typename ShapeDefinitionT,
+        typename SpaceT,
+        typename ServerT,
+        typename TransformT,
+        typename InstanceT,
+        typename BodyStateT>
+    inline void register_physics_instantiation_system(
+        flecs::world& world,
+        const char* system_name,
+        BodyStateT transform_state)
+    {
+        world.system<const ShapesT>(system_name)
+            .kind(flecs::OnUpdate)
+            .without<InstanceT>()
+            .each([transform_state](flecs::iter& it, size_t index, const ShapesT& body_shapes)
+        {
+            flecs::entity entity = it.entity(index);
+            const TransformT* transform = entity.try_get<TransformT>();
+            TransformT initial_transform = transform ? *transform : TransformT();
+            try_create_physics_body<
+                ShapesT,
+                ShapeDefinitionT,
+                SpaceT,
+                ServerT,
+                TransformT,
+                InstanceT>(
+                    it.world(),
+                    entity,
+                    body_shapes,
+                    initial_transform,
+                    transform_state);
+        });
+    }
+
+    template<typename VelocityT, typename InstanceT, typename ServerT, typename BodyStateT>
+    inline void register_physics_velocity_update_system(
+        flecs::world& world,
+        const char* system_name,
+        BodyStateT velocity_state)
+    {
+        world.system<const VelocityT, const InstanceT>(system_name)
+            .kind(flecs::PostUpdate)
+            .each([velocity_state](const VelocityT& velocity, const InstanceT& instance)
+        {
+            ServerT* physics_server = ServerT::get_singleton();
+            if (!physics_server) { return; }
+            if (!instance.body_rid.is_valid()) { return; }
+            physics_server->body_set_state(instance.body_rid, velocity_state, velocity.value);
+        });
+    }
+
+    template<
+        typename InstanceT,
+        typename PositionT,
+        typename TransformComponentT,
+        typename RotationT,
+        typename ScaleT,
+        typename VelocityT,
+        typename ServerT,
+        typename BodyStateT,
+        typename TransformAssignmentFn,
+        typename VelocityAssignmentFn>
+    inline void register_physics_feedback_system(
+        flecs::world& world,
+        const char* system_name,
+        BodyStateT transform_state,
+        BodyStateT velocity_state,
+        godot::Variant::Type transform_variant_type,
+        godot::Variant::Type velocity_variant_type,
+        TransformAssignmentFn assign_transform,
+        VelocityAssignmentFn assign_velocity)
+    {
+        world.system<
+            InstanceT,
+            PositionT,
+            TransformComponentT,
+            RotationT*,
+            ScaleT*,
+            VelocityT*>(system_name)
+            .kind(flecs::PreUpdate)
+            .term_at(4).optional()
+            .term_at(5).optional()
+            .term_at(6).optional()
+            .each([transform_state,
+                velocity_state,
+                transform_variant_type,
+                velocity_variant_type,
+                assign_transform,
+                assign_velocity](
+                    flecs::iter& it,
+                    size_t index,
+                    InstanceT& instance,
+                    PositionT& position,
+                    TransformComponentT& transform_component,
+                    RotationT* rotation,
+                    ScaleT* scale,
+                    VelocityT* velocity)
+        {
+            ServerT* physics_server = ServerT::get_singleton();
+            if (!physics_server) { return; }
+            if (!instance.body_rid.is_valid()) { return; }
+
+            godot::Variant transform_value = physics_server->body_get_state(
+                instance.body_rid,
+                transform_state);
+            if (transform_value.get_type() == transform_variant_type)
+            {
+                assign_transform(transform_value, position, transform_component, rotation, scale);
+            }
+
+            if (velocity)
+            {
+                godot::Variant velocity_value = physics_server->body_get_state(
+                    instance.body_rid,
+                    velocity_state);
+                if (velocity_value.get_type() == velocity_variant_type)
+                {
+                    assign_velocity(velocity_value, *velocity);
+                }
+            }
+        });
+    }
 }
 
 inline FlecsRegistry register_physics_systems([](flecs::world& world)
 {
+    // 2D Physics Systems
+    register_physics_instantiation_system<
+        PhysicsBodyShapes2D,
+        PhysicsBodyShape2DDefinition,
+        PhysicsSpace2D,
+        godot::PhysicsServer2D,
+        godot::Transform2D,
+        PhysicsBodyInstance2D>(
+            world,
+            "Physics Body 2D Instantiation",
+            godot::PhysicsServer2D::BODY_STATE_TRANSFORM);
+
     register_physics_sync_system<godot::Transform2D, PhysicsBodyInstance2D, godot::PhysicsServer2D>(
         world,
         "Physics Body 2D Sync",
         godot::PhysicsServer2D::BODY_STATE_TRANSFORM);
+
+    register_physics_velocity_update_system<
+        Velocity2D,
+        PhysicsBodyInstance2D,
+        godot::PhysicsServer2D>(
+            world,
+            "Physics Body 2D Velocity Update",
+            godot::PhysicsServer2D::BODY_STATE_LINEAR_VELOCITY);
+
+    register_physics_feedback_system<
+        PhysicsBodyInstance2D,
+        Position2D,
+        godot::Transform2D,
+        Rotation2D,
+        Scale2D,
+        Velocity2D,
+        godot::PhysicsServer2D>(
+            world,
+            "Physics Body 2D Feedback",
+            godot::PhysicsServer2D::BODY_STATE_TRANSFORM,
+            godot::PhysicsServer2D::BODY_STATE_LINEAR_VELOCITY,
+            godot::Variant::TRANSFORM2D,
+            godot::Variant::VECTOR2,
+            [](const godot::Variant& transform_value,
+                Position2D& position,
+                godot::Transform2D& transform_component,
+                Rotation2D* rotation,
+                Scale2D* scale)
+    {
+        godot::Transform2D physics_transform = transform_value;
+        transform_component = physics_transform;
+        position.value = physics_transform.get_origin();
+        if (rotation) { rotation->value = physics_transform.get_rotation(); }
+        if (scale) { scale->value = physics_transform.get_scale(); }
+    },
+            [](const godot::Variant& velocity_value, Velocity2D& velocity_component)
+    {
+        godot::Vector2 physics_velocity = velocity_value;
+        velocity_component.value = physics_velocity;
+    });
+
     register_physics_cleanup_observer<PhysicsBodyInstance2D, godot::PhysicsServer2D>(
         world,
         "Physics Body 2D Cleanup");
 
-    world.system<const PhysicsBodyShapes2D>("Physics Body 2D Instantiate")
-        .kind(flecs::OnUpdate)
-        .without<PhysicsBodyInstance2D>()
-        .each([](flecs::iter& it, size_t i, const PhysicsBodyShapes2D& body_shapes)
-    {
-        flecs::entity entity = it.entity(i);
-        const godot::Transform2D* transform = entity.try_get<godot::Transform2D>();
-        godot::Transform2D initial_transform = transform ? *transform : godot::Transform2D();
-        try_create_physics_body<
-            PhysicsBodyShapes2D,
-            PhysicsBodyShape2DDefinition,
-            PhysicsSpace2D,
-            godot::PhysicsServer2D,
-            godot::Transform2D,
-            PhysicsBodyInstance2D>(
-                it.world(),
-                entity,
-                body_shapes,
-                initial_transform,
-                godot::PhysicsServer2D::BODY_STATE_TRANSFORM);
-    });
 
-    world.system<const Velocity2D, const PhysicsBodyInstance2D>("Physics Body 2D Apply Velocity")
-        .kind(flecs::PostUpdate)
-        .each([](const Velocity2D& velocity, const PhysicsBodyInstance2D& instance)
-    {
-        godot::PhysicsServer2D* physics_server = godot::PhysicsServer2D::get_singleton();
-        if (!physics_server) { return; }
-        if (!instance.body_rid.is_valid()) { return; }
-        physics_server->body_set_state(instance.body_rid, godot::PhysicsServer2D::BODY_STATE_LINEAR_VELOCITY, velocity.value);
-    });
-
-    world.system<
-        PhysicsBodyInstance2D,
-        Position2D,
-        godot::Transform2D,
-        Rotation2D*,
-        Scale2D*,
-        Velocity2D*>("Physics Body 2D Feedback")
-        .kind(flecs::PreUpdate)
-        .term_at(4).optional()
-        .term_at(5).optional()
-        .term_at(6).optional()
-        .each([](
-            flecs::iter& it,
-            size_t i,
-            PhysicsBodyInstance2D& instance,
-            Position2D& position,
-            godot::Transform2D& transform_component,
-            Rotation2D* rotation,
-            Scale2D* scale,
-            Velocity2D* velocity)
-    {
-        godot::PhysicsServer2D* physics_server = godot::PhysicsServer2D::get_singleton();
-        if (!physics_server) { return; }
-        if (!instance.body_rid.is_valid()) { return; }
-
-        godot::Variant transform_state = physics_server->body_get_state(
-            instance.body_rid,
-            godot::PhysicsServer2D::BODY_STATE_TRANSFORM);
-        if (transform_state.get_type() == godot::Variant::TRANSFORM2D)
-        {
-            godot::Transform2D physics_transform = transform_state;
-            transform_component = physics_transform;
-            position.value = physics_transform.get_origin();
-            if (rotation) { rotation->value = physics_transform.get_rotation(); }
-            if (scale) { scale->value = physics_transform.get_scale(); }
-        }
-
-        if (velocity)
-        {
-            godot::Variant velocity_state = physics_server->body_get_state(
-                instance.body_rid,
-                godot::PhysicsServer2D::BODY_STATE_LINEAR_VELOCITY);
-            if (velocity_state.get_type() == godot::Variant::VECTOR2)
-            {
-                godot::Vector2 physics_velocity = velocity_state;
-                velocity->value = physics_velocity;
-            }
-        }
-    });
+    // 3D Physics Systems
+    register_physics_instantiation_system <
+        PhysicsBodyShapes3D,
+        PhysicsBodyShape3DDefinition,
+        PhysicsSpace3D,
+        godot::PhysicsServer3D,
+        godot::Transform3D,
+        PhysicsBodyInstance3D>(
+            world,
+            "Physics Body 3D Instantiation",
+            godot::PhysicsServer3D::BODY_STATE_TRANSFORM);
 
     register_physics_sync_system<godot::Transform3D, PhysicsBodyInstance3D, godot::PhysicsServer3D>(
         world,
         "Physics Body 3D Sync",
         godot::PhysicsServer3D::BODY_STATE_TRANSFORM);
+
+    register_physics_velocity_update_system<
+        Velocity3D,
+        PhysicsBodyInstance3D,
+        godot::PhysicsServer3D>(
+            world,
+            "Physics Body 3D Velocity Update",
+            godot::PhysicsServer3D::BODY_STATE_LINEAR_VELOCITY);
+
+    register_physics_feedback_system<
+        PhysicsBodyInstance3D,
+        Position3D,
+        godot::Transform3D,
+        Rotation3D,
+        Scale3D,
+        Velocity3D,
+        godot::PhysicsServer3D>(
+            world,
+            "Physics Body 3D Feedback",
+            godot::PhysicsServer3D::BODY_STATE_TRANSFORM,
+            godot::PhysicsServer3D::BODY_STATE_LINEAR_VELOCITY,
+            godot::Variant::TRANSFORM3D,
+            godot::Variant::VECTOR3,
+            [](const godot::Variant& transform_value,
+                Position3D& position,
+                godot::Transform3D& transform_component,
+                Rotation3D* rotation,
+                Scale3D* scale)
+    {
+        godot::Transform3D physics_transform = transform_value;
+        transform_component = physics_transform;
+        position.value = physics_transform.origin;
+        if (rotation) { rotation->value = physics_transform.basis.get_euler(); }
+        if (scale) { scale->value = physics_transform.basis.get_scale(); }
+    },
+            [](const godot::Variant& velocity_value, Velocity3D& velocity_component)
+    {
+        godot::Vector3 physics_velocity = velocity_value;
+        velocity_component.value = physics_velocity;
+    });
+
     register_physics_cleanup_observer<PhysicsBodyInstance3D, godot::PhysicsServer3D>(
         world,
         "Physics Body 3D Cleanup");
-
-    world.system<const PhysicsBodyShapes3D>("Physics Body 3D Instantiate")
-        .kind(flecs::OnUpdate)
-        .without<PhysicsBodyInstance3D>()
-        .each([](flecs::iter& it, size_t i, const PhysicsBodyShapes3D& body_shapes)
-    {
-        flecs::entity entity = it.entity(i);
-        const godot::Transform3D* transform = entity.try_get<godot::Transform3D>();
-        godot::Transform3D initial_transform = transform ? *transform : godot::Transform3D();
-        try_create_physics_body<
-            PhysicsBodyShapes3D,
-            PhysicsBodyShape3DDefinition,
-            PhysicsSpace3D,
-            godot::PhysicsServer3D,
-            godot::Transform3D,
-            PhysicsBodyInstance3D>(
-                it.world(),
-                entity,
-                body_shapes,
-                initial_transform,
-                godot::PhysicsServer3D::BODY_STATE_TRANSFORM);
-    });
 });
