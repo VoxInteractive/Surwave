41095a5a8fe4cb4dbf10e87f8757e024eee736cd
Alp <gokalp84@gmail.com>
2025-11-21 13:30:43 +1100

    include Position2D in query

--- Game/cpp/systems/enemy_collision_resolution.h
+++ Game/cpp/systems/enemy_collision_resolution.h
@@ -17,12 +17,13 @@
 
 
 inline FlecsRegistry register_enemy_collision_resolution_system([](flecs::world& world) {
-    world.system<Velocity2D, const RadiusSquared>("Enemy Collision Resolution")
+    world.system<Velocity2D, const Position2D, const RadiusSquared>("Enemy Collision Resolution")
         .with(flecs::IsA, world.lookup("Enemy"))
         .kind(flecs::OnValidate)
         .each([](
             flecs::entity entity,
             Velocity2D& velocity,
+            const Position2D& position,
             const RadiusSquared& radius_sq) {
 
         // TODO: Implement enemy collision resolution logic here
