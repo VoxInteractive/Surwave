d9e470f0ed8937a1845779fca92e90ae26d66e0e
Alp <gokalp84@gmail.com>
2025-11-22 10:30:50 +1100

    replace all enemy movement logic and states with a boids-based implementation

--- Game/cpp/components/enemy.h
+++ Game/cpp/components/enemy.h
@@ -4,10 +4,6 @@
 
 #include "src/flecs_registry.h"
 
-struct RadiusSquared {
-    godot::real_t value;
-};
-
 struct HitPoints {
     godot::real_t value;
 };
@@ -20,27 +16,8 @@ struct MeleeDamage {
     godot::real_t value;
 };
 
-struct PlayerDetectionRadiusSquared {
-    godot::real_t value;
-};
-
-struct LoseTargetRadiusSquared {
-    godot::real_t value;
-};
-
-struct WanderMoveDuration {
-    godot::real_t value;
-};
-
-struct RestDuration {
-    godot::real_t value;
-};
-
 
 inline FlecsRegistry register_enemy_stats_components([](flecs::world& world) {
-    world.component<RadiusSquared>("RadiusSquared")
-        .member<godot::real_t>("value");
-
     world.component<HitPoints>("HitPoints")
         .member<godot::real_t>("value");
 
@@ -48,17 +25,5 @@ inline FlecsRegistry register_enemy_stats_components([](flecs::world& world) {
         .member<godot::real_t>("value");
 
     world.component<MeleeDamage>("MeleeDamage")
-        .member<godot::real_t>("value");
-
-    world.component<PlayerDetectionRadiusSquared>("PlayerDetectionRadiusSquared")
-        .member<godot::real_t>("value");
-
-    world.component<LoseTargetRadiusSquared>("LoseTargetRadiusSquared")
-        .member<godot::real_t>("value");
-
-    world.component<WanderMoveDuration>("WanderMoveDuration")
-        .member<godot::real_t>("value");
-
-    world.component<RestDuration>("RestDuration")
         .member<godot::real_t>("value");
 });
