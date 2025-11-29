c2943c664c81aba693a76a41e6eb91c74509e689
60k41p <gokalp84@gmail.com>
2025-11-30 10:34:02 +1100

    Optimise enemy movement (#32)
    * simplify the movement code & make it twice as faster
    
    * simplify further
    
    * add back grid_cell_size
    
    * balanced separation & player attraction weights

--- Game/cpp/components/singletons.h
+++ Game/cpp/components/singletons.h
@@ -6,12 +6,6 @@
 #include "src/flecs_registry.h"
 #include "src/flecs_singleton_registry.h"
 
-struct EnemyBoidForceWeights {
-    godot::real_t alignment_weight;
-    godot::real_t cohesion_weight;
-    godot::real_t separation_weight;
-};
-
 struct EnemyBoidMovementSettings {
     godot::real_t player_attraction_weight;
     godot::real_t player_engage_distance;
@@ -20,6 +14,7 @@ struct EnemyBoidMovementSettings {
     godot::real_t max_speed_multiplier;
     godot::real_t max_force;
     godot::real_t grid_cell_size;
+    godot::real_t separation_weight;
     godot::real_t kd_tree_rebuild_distance;
     godot::real_t kd_tree_max_stale_frames;
     godot::real_t max_neighbor_sample_count;
@@ -70,17 +65,6 @@ struct EnemyCount
 
 
 inline FlecsRegistry register_game_singleton_components([](flecs::world& world) {
-    world.component<EnemyBoidForceWeights>("EnemyBoidForceWeights")
-        .member<godot::real_t>("alignment_weight")
-        .member<godot::real_t>("cohesion_weight")
-        .member<godot::real_t>("separation_weight")
-        .add(flecs::Singleton)
-        .set<EnemyBoidForceWeights>({
-            godot::real_t(0.8),   // alignment_weight
-            godot::real_t(0.45),  // cohesion_weight
-            godot::real_t(1.1)   // separation_weight
-            });
-
     world.component<EnemyBoidMovementSettings>("EnemyBoidMovementSettings")
         .member<godot::real_t>("player_attraction_weight")
         .member<godot::real_t>("player_engage_distance")
@@ -89,23 +73,25 @@ inline FlecsRegistry register_game_singleton_components([](flecs::world& world) 
         .member<godot::real_t>("max_speed_multiplier")
         .member<godot::real_t>("max_force")
         .member<godot::real_t>("grid_cell_size")
+        .member<godot::real_t>("separation_weight")
         .member<godot::real_t>("kd_tree_rebuild_distance")
         .member<godot::real_t>("kd_tree_max_stale_frames")
         .member<godot::real_t>("max_neighbor_sample_count")
         .member<godot::real_t>("separation_noise_intensity")
         .add(flecs::Singleton)
         .set<EnemyBoidMovementSettings>({
-            godot::real_t(0.5),   // player_attraction_weight
+            godot::real_t(1.0),   // player_attraction_weight
             godot::real_t(28.0),  // player_engage_distance
             godot::real_t(110.0), // neighbor_radius
-            godot::real_t(40.0),  // separation_radius
+            godot::real_t(30.0),  // separation_radius
             godot::real_t(1.1),   // max_speed_multiplier
             godot::real_t(220.0), // max_force
             godot::real_t(96.0),  // grid_cell_size
+            godot::real_t(1.0),   // separation_weight
             godot::real_t(35.0),  // kd_tree_rebuild_distance
             godot::real_t(8.0),   // kd_tree_max_stale_frames
             godot::real_t(48.0),  // max_neighbor_sample_count
-            godot::real_t(0.05)    // separation_noise_intensity
+            godot::real_t(0.05)   // separation_noise_intensity
             });
 
     world.component<EnemyAnimationSettings>("EnemyAnimationSettings")
