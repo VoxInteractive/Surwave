6771d2f9a6da96d7a58395dfa1d686e212fcfe59
Alp <gokalp84@gmail.com>
2025-12-01 13:58:17 +1100

    credits screen

--- Game/scenes/ui/main_menu.gd
+++ Game/scenes/ui/main_menu.gd
@@ -1,10 +1,12 @@
 extends CanvasLayer
 
-var options_menu_scene = preload("res://scenes/ui/options_menu.tscn")
+const OPTIONS_MENU = preload("uid://crlhaxk0hrl4f")
+const CREDITS_SCREEN = preload("uid://dq8qlbgcgavbj")
 
 func _ready() -> void:
 	$%PlayButton.pressed.connect(_on_play_pressed)
 	$%OptionsButton.pressed.connect(_on_options_pressed)
+	$%CreditsButton.pressed.connect(_on_credits_pressed)
 	$%QuitButton.pressed.connect(_on_quit_pressed)
 	
 	AudioManager.intro.play()
@@ -19,9 +21,17 @@ func _on_play_pressed() -> void:
 func _on_options_pressed() -> void:
 	ScreenTransition.transition()
 	await ScreenTransition.transitioned_halfway
-	var options_menu_instance = options_menu_scene.instantiate()
+	var options_menu_instance = OPTIONS_MENU.instantiate()
 	add_child(options_menu_instance)
 	options_menu_instance.back_button_pressed.connect(_on_options_menu_closed.bind(options_menu_instance))
+
+
+func _on_credits_pressed() -> void:
+	ScreenTransition.transition()
+	await ScreenTransition.transitioned_halfway
+	var credits_menu_instance = CREDITS_SCREEN.instantiate()
+	add_child(credits_menu_instance)
+	credits_menu_instance.back_button_pressed.connect(_on_options_menu_closed.bind(credits_menu_instance))
 
 
 func _on_quit_pressed() -> void:
